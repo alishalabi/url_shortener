136e459e172f7feed5f318fcea8a1d6217a70915
Ali Shalabi <ashalabi13@gmail.com>
2019-11-11 12:33:39 -0800

    Have written routes + router, working on handlers

---
+++ routes.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+  "net/http"
+)
+
+type Route struct {
+  Name string
+  Method string
+  Pattern string
+  HandlerFunc http.HandlerFunc
+}
+
+// Instantiate empty routes slice
+type Routes []Route
+
+// Populate that slice with the routes and urls we want
+func CreateRoutes(LS *LinkShortnerAPI) Routes {
+  return Routes{
+    Route {
+      "UrlRoot",
+      "GET",
+      "/",
+      LS.UrlRoot,
+    },
+    Route {
+      "UrlShow",
+      "GET",
+      "/{shorturl}",
+      LS.UrlShow,
+    },
+    Route {
+      "UrlCreate",
+      "POST",
+      "/Create",
+      LS.UrlCreate,
+    },
+  }
+}
